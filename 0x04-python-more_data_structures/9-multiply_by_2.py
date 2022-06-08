4e510a17af5d06560072dc74b21428d6aa7176a0
MDoreen <dmachoni@gmail.com>
2022-06-08 16:21:31 -0700

    A fuction allowing multiplication of 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    keys = list(a_dictionary.keys())
+    new_dir = a_dictionary.copy()
+    list_keys = list(new_dir.keys())
 
-    for i in keys:
-        a_dictionary[i] *= 2
+    for i in list_keys:
+        new_dir[i] *= 2
 
-    return (a_dictionary)
+    return (new_dir)
